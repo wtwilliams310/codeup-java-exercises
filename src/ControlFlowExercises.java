5bd4f70b77b0b66d690ed99e3f43b37494498cb1
Titus Williams <wtwilliams310@gmail.com>
2023-02-10 13:27:01 -0600

    completed Control Statements exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class ControlFlowExercises {
@@ -37,7 +38,9 @@ public class ControlFlowExercises {
 
         // 1.b.iii
 
-//        double x = 2;
+//        long x = 2;
+//
+//
 //
 //        do {
 //            System.out.println(x);
@@ -56,25 +59,81 @@ public class ControlFlowExercises {
 
         //2. FizzBuzz
 
-        int i = 1;
+//        int i = 1;
+//
+//        for(i = 1; i <= 100; i++) {
+//            if(i % 3 == 0 && i % 5 == 0){
+//                System.out.printf("FizzBuzz%n");
+//            } else if(i % 5 == 0 && i % 3 != 0){
+//                System.out.printf("Buzz%n");
+//
+//
+//            } else if(i % 3 == 0 && i % 5 != 0){
+//                System.out.printf("Fizz%n");
+//
+//            } else {
+//                System.out.println(i);
+//
+//            }
+//
+//
+//        }
+        // #3 Display a table of powers
+//        int i;
+//        boolean confirmation;
+//
+//        do {
+//            System.out.print("Enter an integer: ");
+//            int input= scanner.nextInt();
+//            for(i = 1; i <= input; i++) {
+//                int result = i * i;
+//                int cubed = result * i;
+//                if(i == 1){
+//                    System.out.printf("Here is your table!%n" +
+//                            "%n" +
+//                            "number | squared | cubed" +
+//                            "%n------ | ------- | ------%n" +
+//                            "%s      | %s       | %s%n", i, result, cubed);
+//
+//
+//                } else if(i > 1 && i <= 3) {
+//                    System.out.printf("%s      | %s       | %s%n", i, result, cubed);
+//                }
+//                else if (i > 3){
+//                    System.out.printf("%s      | %s      | %s%n", i, result, cubed);
+//                }
+//
+//            }
+//            System.out.println("Would you like to continue?");
+//            String response = scanner.next();
+//
+//            confirmation = response.equals("y");
+//
+//        } while(confirmation);
 
-        for(i = 1; i <= 100; i++) {
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.printf("FizzBuzz%n");
-            } else if(i % 5 == 0 && i % 3 != 0){
-                System.out.printf("Buzz%n");
 
-
-            } else if(i % 3 == 0 && i % 5 != 0){
-                System.out.printf("Fizz%n");
-
+        boolean confirmation;
+        do {
+            System.out.print("Enter a grade: ");
+            int input = scanner.nextInt();
+            if(input >= 88) {
+                System.out.println("Grade = A");
+            } else if(input >= 80 || input <= 87){
+                System.out.println("Grade = B");
+            } else if(input >= 67 || input <= 79){
+                System.out.println("Grade = C");
+            } else if(input >= 60 || input <= 66){
+                System.out.println("Grade = D");
             } else {
-                System.out.println(i);
+                System.out.println("Grade = F");
 
             }
 
+            System.out.println("Would you like to continue? [y/n]");
+            String response = scanner.next();
 
-        }
+            confirmation = response.equals("y");
+        }while(confirmation);
 
 
     }
