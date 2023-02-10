150c4acc2d6203543032d5e7e0151e2100789fe9
Titus Williams <wtwilliams310@gmail.com>
2023-02-10 17:17:52 -0600

    updated Strings exercises

--- src/Bob.java
+++ src/Bob.java
@@ -5,30 +5,28 @@ public class Bob {
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Talk to Bob...");
-
         String input = scanner.nextLine();
-        int count = 0;
-for(int i = 0; i >= count; i++) {
-    if(input.endsWith("?")) {
-        System.out.println("Sure.");
-        input = scanner.next();
-        count++;
+        int count = 1;
+        for (int i = 0; i < count; i++) {
+            if (input.endsWith("?")) {
+                System.out.println("Sure.");
+                input = scanner.nextLine();
+                count++;
+            } else if (input.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+                input = scanner.nextLine();
+                count++;
+            }  else if (input.equals("")) {
+                System.out.println("Whatever.");
+                count++;
+                input = scanner.nextLine();
+            } else if (input.equals("bye")) {
+                System.out.println("Please come back!!!");
+//                count = -1;
+            } else  {
+                System.out.println("Fine. Be that way!");
+                count++;
+                input = scanner.nextLine();
 
-    } if (input.endsWith("!")) {
-        System.out.println("Whoa, chill out!");
-        input = scanner.next();
-        count++;
-
-
-    } if (input.equals("")) {
-        input = scanner.next();
-
-        System.out.println("Whatever.");
-        count = 10;
-    }
-}
-
-
-    }
-}
+            }
+        }}}
